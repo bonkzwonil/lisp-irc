e10c79293200eb3d647a60cc917f6bfe1fbcaae7
matze <matze@mausland.de>
2007-10-29 22:50:50 +0100

    demobot load file which loads in sbcl and clisp (detects automatically the lisp version)
    darcs-hash:20071029215050-fa335-cce30d266c3f20e3c0d4c342bc55007725b853b4

--- clisp-demo.lisp
+++ clisp-demo.lisp
@@ -1,14 +1,34 @@
 ; So lüppt das in clisp
 ; sollte nur leicht angepasst werden müssen
 
+(format t "~%~%~%~%~%~%~%~%~%~%~%~%Remember to adjust paths in demo.lisp!~%~%~%~%~%~%~%~%~%~%~%~%~%")
+
+
 ; ASDF laden (bei clisp nich dabei wie bei sbcl)
-(load "/Users/matze/asdf/asdf.lisp")
+#+clisp   (load "/Users/matze/asdf/asdf.lisp")
+
+;; CLISP loading code
+#+clisp 
+ (progn 
+   (format t "loading in CLISP~%~%")
 ; ASDF systems path (ich nehm den von sbcl (warum alles mehrfach installen))
-(push "/Users/matze/.sbcl/systems/" asdf:*central-registry*)
+   (push "/Users/matze/.sbcl/systems/" asdf:*central-registry*)
 ;matzlisp laden
-(asdf:oos 'asdf:load-op 'matzlisp) ; (require 'matzlisp) in sbcl
+   (asdf:oos 'asdf:load-op 'matzlisp) ; (require 'matzlisp) in sbcl
 ; split-sequence laden
-(asdf:oos 'asdf:load-op 'split-sequence)
+   (asdf:oos 'asdf:load-op 'split-sequence))
+
+;SBCL loading code 
+#+sbcl (progn
+	 (format t "loading in SBCL~%~%")
+	 (require 'asdf)
+	 (require 'matzlisp)
+	 (require 'split-sequence))
+	 
+
+
+;common shit
+
 ;irc lib
 (load "irc.lisp")
 ;demobot
