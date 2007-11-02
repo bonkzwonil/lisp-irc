192d42fcff36bc00a812832b0793f7209d08a990
matze <matze@mausland.de>
2007-11-02 11:46:21 +0100

    tests noch etwas besser
    darcs-hash:20071102104621-fa335-e5142a1537fc89ccbe333139af505d002725e1a2

--- tests.lisp
+++ tests.lisp
@@ -7,16 +7,23 @@
 
 (defvar runbot nil)
 
+(time
 (let ((runbot nil))
   (handler-case 
       (progn
-	(format t "Testing demobot...")
+	(format t "~%Testing demobot...~%")
+	(force-output)
 	(with-output-to-string (*standard-output*)
 	  (load "demo-loader.lisp"))
+	(format t "~%Test compile of boersi...~%")
+	(force-output)
+	(with-output-to-string (*standard-output*)
+	  (compile-file "newirc.lisp")
+	  (compile-file "boersenbot.lisp"))
 	)
     (error (e) (progn (format t "KACK: ~A~%" e) 
 		      #+clisp (quit 1)
-		      #+sbcl (quit :unix-status 1)))))
+		      #+sbcl (quit :unix-status 1))))))
 
-(format t "Geilo es scheint keine errors gegeben zu haben")
+(format t "~%Geilo es scheint keine errors gegeben zu haben~%")
 (quit)
