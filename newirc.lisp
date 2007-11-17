d488fdefe78bbcdd395d8c2dc70c0c6dd987b2b8
matze <matze@mausland.de>
2007-11-17 22:13:25 +0100

    rewrite 3
    darcs-hash:20071117211325-fa335-9960ba447a36110568566c9959757d4de29a0451

--- newirc.lisp
+++ newirc.lisp
@@ -1,9 +1,5 @@
-;; IRC Library Rewrite 2
+;; IRC Library Rewrite 3
 ;; C 2007 Matzo
-
-
-;; $Id: newirc.lisp,v 1.6 2007/10/10 17:24:09 matze Exp $
-
 
 
 
@@ -27,11 +23,12 @@
 	   
 	   
    
+
   
 
 (in-package :irc)
 
-(defparameter +version+ "2nd Rewrite $Revision: 1.6 $") 
+(defparameter +version+ "3rd Rewrite Version 0.8") 
 
 ;; Codes		      
 (defconstant RPL_ENDOFMOTD 376)
@@ -39,134 +36,70 @@
 (defconstant ERR_NICKINUSE 433)
 
 
-(defstruct irc 
-  server port nick user tcpstream codehooks commandhooks actions)
+;; Back to CLOS
 
-(defstruct message
-  code command source target argument raw irc)
+(defclass ircbot ()
+  ((nick
+    :initarg :nick
+    :reader nick
+    :type string)
+   (username
+    :initarg :username
+    :reader username
+    :type string)
+   (telnet-connection
+    :initarg :telnet-connection
+    :reader telnet-connection)
+   
+   (codehooks
+    :initform nil)
+   (commandhooks
+    :initform nil)
+   (actions
+    :initform (make-hash-table :test #'equal))))
 
-;; Top handler
-
-(defun handle-line (irc line)
-  (handle-msg irc (parsemessage line)))
-
-
-(defun handle-msg (irc msg)
-  (invoke-code-hooks irc msg)
-  (invoke-command-hooks irc msg)
-  (invoke-actions irc msg))
-
-;; Codehooks
-
-(defun invoke-code-hooks (irc msg)
-  (mapcar #'(lambda (hook) (if (= (message-code msg) (getf hook :code)) (funcall (getf hook :fun)  irc msg)))
-	  (irc-codehooks irc)))
-
-(defun add-code-hook (irc code fun)
-  (setf (irc-codehooks irc)
-	(cons (list :code code :fun fun) (irc-codehooks irc))))
-
-;; Commandhooks
-(defun invoke-command-hooks (irc msg)
-  (mapcar #'(lambda (hook) (if (string= (message-command msg) (getf hook :command)) (funcall (getf hook :fun) irc msg)))
-	  (irc-commandhooks irc)))
-
-(defun add-command-hook (irc command fun)
-  (setf (irc-commandhooks irc)
-	(cons (list :command command :fun fun) (irc-commandhooks irc))))
-
-
-;; Action Hooks system
-
-
-(defun add-action (irc regexp function doc &key (private nil) (needs-caller nil) (hidden nil) (needs-raw nil))
-  "Erstellt eine Aktion die auf regexp hört und function ausführt. bei private=T wird die antwort privat verschickt."
-  (if (null (irc-actions irc)) (setf (irc-actions irc) (make-hash-table :test #'equal)))
-  (setf (gethash regexp (irc-actions irc)) (list :function function :doc doc :private private :needs-caller needs-caller :hidden hidden :needs-raw needs-raw) ))
+(defclass ircmessage ()
+  ((code 
+    :initarg :code 
+    :reader code)
+   (command 
+    :initarg :command 
+    :reader command)
+   (source 
+    :initarg :source 
+    :reader source)
+   (target 
+    :initarg :target 
+    :reader target)
+   (argument 
+    :initarg :argument 
+    :reader argument)
+   (raw 
+    :initarg :raw 
+    :reader raw)))
 
 
-(defun remove-action (irc regexp)
-  "löscht alle Aktionen mit regexp"
-  (remhash regexp (irc-actions irc)))
-
-(defun invoke-action (irc regexp msg)
-  "fuehrt action aus wenn ihre regexp in message vorkommt"
-  (let ((match (cl-ppcre:scan-to-strings regexp (message-argument msg))))
-    (if match
-	(let* ((action (gethash regexp (irc-actions irc)))
-	      (arg (second (split-sequence:split-sequence #\Space (message-argument msg))))
-	      (arg2 (third (split-sequence:split-sequence #\Space (message-argument msg))))
-	      (arg3 (fourth (split-sequence:split-sequence #\Space (message-argument msg))))
-	      (arg4 (fifth (split-sequence:split-sequence #\Space (message-argument msg))))
-	      (arglist nil)
-	      (needs-caller (getf action :needs-caller))
-	      (needs-raw (getf action :needs-raw)))
-	  ;build arglist
-	  (if arg4 (push arg4 arglist))
-	  (if arg3 (push arg3 arglist))
-	  (if arg2 (push arg2 arglist))
-	  (if arg (push arg arglist))
-	  (if needs-caller (setf arglist (append arglist (list :caller (message-source msg)))))
-	  (if needs-raw (setf arglist (append arglist (list :needs-raw (message-raw msg)))))
-	  (let ((result 
-		 (handler-case 
-		  (apply (getf action :function) arglist) ;; Der eigentliche Funktionsaufruf der action
-		  (error (e) 
-			 ;;perhaps send the error to a user?
-			 (format t "ERROR: ~a~%" e)
-			 (format nil "Usage: ~a -> ~a" regexp (getf action :doc))))))
-	    (send-action-result
-	     irc 
-	     (if (getf action :private) (message-source msg) (if (string= (message-target msg) (irc-nick irc)) (message-source msg) (message-target msg))) 
-	     result))))))
-
-
-(defparameter +senddelay+ 0.5)
-
-(defun send-action-result (irc target lines)
-  (if (listp lines)
-      (mapcar #'(lambda (line)  (privmsg irc target line) (if (> (length line) 0) (sleep +senddelay+)))
-	      lines)
-    (privmsg irc target lines)))
+(defmethod handle ((bot ircbot) (msg ircmessage))
+  (invoke-code-hooks bot (code msg))
+  (invoke-command-hooks bot msg)
+  (invoke-action bot msg))
 
 
 
-(defun invoke-actions (irc msg)
-  (maphash #'(lambda (regexp action)
-	       (invoke-action irc regexp msg))
-	   (irc-actions irc)))
+;; Basic IO
 
-	   
+(defmethod sendline ((bot ircbot) (line string))
+  (matzlisp::telnet-send (telnet-connection bot) line))
 
-
-;; Main run-loop
-  
-(defun run-irc (irc)
-  (matzlisp:run-telnet
-   (setf (irc-tcpstream irc)
-	 (matzlisp:open-telnet-connection 
-	  (irc-server irc) 
-	  (irc-port irc)
-	  #'(lambda (line) (handle-line irc line))
-	  (list
-	   (format nil "NICK ~a" (irc-nick irc))
-	   (format nil "USER ~a localhost.localdomain ~a :~a" (irc-nick irc) (irc-server irc) (irc-nick irc)))))))
-
-  
-  
-
-(defun sendline (irc line)
-  (matzlisp::telnet-send (irc-tcpstream irc) line))
-
-(defun sendcmd (irc cmd arg1 &optional arg2)
+(defmethod sendcmd ((bot ircbot) cmd arg1 &optional arg2)
   (if arg2
-      (sendline irc (format nil "~a ~a :~a" cmd arg1 arg2))
-    (sendline irc (format nil "~a ~a" cmd arg1))))
+      (sendline bot (format nil "~a ~a :~a" cmd arg1 arg2))
+    (sendline bot (format nil "~a ~a" cmd arg1))))
 
 ;; a nice commandcreator without bloat
 (defun make-cmd (cmd)
-  `(defun ,cmd (irc arg &optional (arg2 nil))
-     (sendcmd irc (string ',cmd) arg (if arg2 arg2))))
+  `(defun ,cmd (bot arg &optional (arg2 nil))
+     (sendcmd bot (string ',cmd) arg (if arg2 arg2))))
        
 
 ;;Build a bunch of commands 
@@ -177,6 +110,147 @@
 
 
 
+
+(defparameter +senddelay+ 0.5)
+
+(defmethod send-lines ((bot ircbot) (target string) lines)
+  (if (listp lines)
+      (mapcar #'(lambda (line)  
+		  (privmsg bot target line) 
+		  (if (> (length line) 0) 
+		      (sleep +senddelay+)))
+	      lines)
+    (privmsg bot target lines)))
+
+    
+
+
+
+
+
+;; Codehooks
+
+(defmethod invoke-code-hooks ((bot ircbot) (code integer))
+  (mapcar #'(lambda (hook) (if (= code (getf hook :code)) (funcall (getf hook :fun) bot)))
+	  (slot-value bot 'codehooks)))
+
+(defmethod add-hook ((bot ircbot) (code integer) (fun function))
+  (setf (slot-value bot 'codehooks)
+	(cons (list :code code :fun fun) (slot-value bot 'codehooks))))
+
+;; Commandhooks
+
+(defmethod add-hook ((bot ircbot) (command string) (fun function))
+  (setf (slot-value bot 'commandhooks)
+	(cons (list :command command :fun fun) (slot-value bot 'commandhooks))))
+
+(defmethod invoke-command-hooks ((bot ircbot) (msg ircmessage))
+  (mapcar 
+   #'(lambda (hook) 
+       (if (string= (command msg) (getf hook :command)) 
+	   (funcall (getf hook :fun) bot msg)))
+   (slot-value bot 'commandhooks)))
+
+
+
+;; Action Hooks system
+
+
+(defclass bot-action ()
+  ((function
+    :initarg :function)
+   (doc
+    :initarg :doc)
+   (private 
+    :initarg :private
+    :reader private?)
+   (needs-caller
+    :initarg :needs-caller
+    :reader needs-caller?)
+   (hidden
+    :initarg :hidden
+    :reader hidden?)
+   (needs-raw
+    :initarg :needs-raw
+    :reader needs-raw?)))
+   
+
+(defun add-action (bot prefix function doc &key (private nil) (needs-caller nil) (hidden nil) (needs-raw nil))
+  "Erstellt für bot eine Aktion die auf prefix hört und function ausführt. bei private=T wird die antwort privat verschickt."
+  (setf (gethash prefix (slot-value bot 'actions))
+	(make-instance 'bot-action 
+		       :function function
+		       :doc doc
+		       :private private
+		       :needs-caller needs-caller
+		       :hidden hidden
+		       :needs-raw needs-raw)))
+  
+
+(defmethod remove-action ((bot ircbot) (prefix string))
+  "löscht alle Aktionen mit prefix"
+  (remhash prefix (slot-value bot 'actions)))
+
+  
+(defmethod get-action ((bot ircbot) (msg ircmessage))
+  (let ((actions (slot-value bot 'actions)))
+    (loop for prefix being the hash-keys of actions do
+	  (if (search prefix (argument msg))
+	      (return-from get-action (gethash prefix actions))))))
+
+
+(defmethod invoke-action ((bot ircbot) (msg ircmessage))
+  (let ((action (get-action bot msg)))
+    (if action
+	(let* ((fun (slot-value action 'function))
+	       (arglist (cdr (split-sequence:split-sequence #\Space (argument msg)))))
+	  (if (needs-caller? action)
+	      (setf arglist (append arglist (list :caller (source msg)))))
+	  (if (needs-raw? action)
+	      (setf arglist (append arglist (list :raw (raw msg)))))
+	  (let ((result
+		 (handler-case 
+		  (apply fun arglist) ;; Apply the function
+		  (error (e) ;; catch errors
+			 (format t "ERROR: ~a~%" e)
+			 e))))
+	    (send-lines 
+	     bot 
+	     (if (private? action) 
+		 (source msg) 
+	       (if (string= (target msg) (nick bot))
+		   (source msg)
+		 (target msg)))
+	     result))))))
+      
+		   
+
+;; Top handler
+
+(defmethod handle-line ((bot ircbot) (line string))
+  (handle bot (parsemessage line)))
+
+
+	   
+
+
+;; Main run-loop
+  
+(defmethod run ((bot ircbot) (server string) &optional (port 6666))
+  (let ((connection 
+	 (matzlisp::open-telnet-connection 
+	  server
+	  port
+	  #'(lambda (line) (handle-line bot line))
+	  (list
+	   (format nil "NICK ~a" (nick bot))
+	   (format nil "USER ~a localhost.localdomain ~a :~a" (nick bot) (username bot) (nick bot))))))
+    (setf (slot-value bot 'telnet-connection) connection))
+  (matzlisp:run-telnet (telnet-connection bot)))
+
+
+  
+  
 
 
 ;;Parsing stuff
@@ -199,41 +273,42 @@
     (string-trim " " ret)))
       
 
+(defun irc-parseint (str) ; return 0 on error
+  (handler-case (parse-integer str) (error (e) 0)))
+
+
 
 
 (defun debugircmsg (msg)
-  (format nil "source: ~a, command: ~a, code : ~a, arg: ~a" (message-source msg) (message-command msg) (message-code msg) (message-argument msg)))
+  (format nil "source: ~a, command: ~a, code : ~a, arg: ~a" (source msg) (command msg) (code msg) (argument msg)))
 
 (defun parsemessage (line)
-  (let (
-	(seq 
-	 (split-sequence:split-sequence #\Space line))
-	)
-    (let* ((msg
-	   (make-message 
-		   :source (parse-nick (first seq)) 
-		   :command (second seq)
-		   :argument (string-trim 
+  (let* ((seq 
+	  (split-sequence:split-sequence #\Space line))
+	 
+	 (msg
+	  (make-instance 'ircmessage  
+			 :source (parse-nick (first seq)) 
+			 :command (second seq)
+			 :argument (string-trim 
 				       (string #\Return) 
 				       (string-trim 
 					":" 
 					(string-list-concat 
 					 (cdddr seq) 
 					 " ")))
-		   :code (irc-parseint (second seq))
-		   :target (third seq)
-		   :raw line))
-	  (spos (search ":" (message-command msg))))
-      
-      (if (and (numberp spos) (= 0 spos))
-	  (let ()
-	    (setf (message-argument msg) (message-command msg))
-	    (setf (message-command msg) (message-source msg))
-	    (setf (message-source msg) nil)
-	    ))
-      msg)))
-       
+			 :code (irc-parseint (second seq))
+			 :target (third seq)
+			 :raw line))
+	 (spos (search ":" (command msg))))
+  
+    (if (and (numberp spos) (= 0 spos))
+	(let ()
+	  (setf (slot-value msg 'argument) (command msg))
+	  (setf (slot-value msg 'command) (source msg))
+	  (setf (slot-value msg 'source) nil)))
+    msg))
 
-(defun irc-parseint (str)
-  (handler-case (parse-integer str) (parse-error (e) 0)))
+
+
 
